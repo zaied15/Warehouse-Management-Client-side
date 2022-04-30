805637035ab9b8dc1ab919fe0aebf9451bd332e8
unknown <mda800@gmail.com>
2022-04-30 11:26:35 +0600

    featured item and a extra section added

---
+++ src/pages/Home/FeaturedCar/FeaturedCar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./FeaturedCar.css";
+
+const FeaturedCar = ({ car }) => {
+  return (
+    <div className="col">
+      <div className="card h-100 border-bottom-0">
+        <div className="position-relative">
+          <img
+            src={car.img}
+            height="290px"
+            className="card-img-top"
+            alt={car.name}
+          />
+          <span className="position-absolute d-inline-block bottom-0 end-0 price">
+            Price: ${car.price}
+          </span>
+        </div>
+        <div className="card-body">
+          <h5 className="card-title fw-bold text-danger text-center mb-4">
+            {car.name}
+          </h5>
+          <div className="d-flex justify-content-between">
+            <h6 className="card-title bg-success p-2 rounded text-light">
+              Supplier: {car.supplier}
+            </h6>
+            <h6 className="card-title bg-info p-2 rounded text-light">
+              Quantity: {car.quantity}
+            </h6>
+          </div>
+          <p className="card-text mt-3">{car.description}</p>
+        </div>
+        <div className="card-footer border-0 p-0">
+          <button className="w-100 btn btn-danger">Manage Inventory</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedCar;
--- src/pages/Home/FeaturedItems/FeaturedItems.js
+++ src/pages/Home/FeaturedItems/FeaturedItems.js
@@ -1,10 +1,12 @@
 import React from "react";
 import useCars from "../../../hooks/useCars";
-import FeaturedCar from "../../FeaturedCar/FeaturedCar";
+import FeaturedCar from "../FeaturedCar/FeaturedCar";
 import "./FeaturedItems.css";
 
 const FeaturedItems = () => {
   const [cars] = useCars();
+  const stockOut = cars.filter((car) => car.quantity === 0);
+  const inStock = cars.filter((car) => car.quantity > 0);
 
   return (
     <div className="container">
@@ -14,10 +16,23 @@ const FeaturedItems = () => {
             FEATURED PRODUCTS
           </span>
         </h3>
-        <div class="row row-cols-1 row-cols-md-3 g-4">
-          {cars.slice(0, 6).map((car) => (
+        <div className="row row-cols-1 row-cols-md-3 g-4 mb-5">
+          {inStock.slice(0, 6).map((car) => (
             <FeaturedCar car={car}></FeaturedCar>
           ))}
+        </div>
+        <hr />
+        <div>
+          <h3 className="section-title text-center my-5 fw-bold">
+            <span className="d-inline-block border-bottom border-danger border-2">
+              STOCK OUT PRODUCTS
+            </span>
+          </h3>
+          <div className="row row-cols-1 row-cols-md-3 g-4">
+            {stockOut.map((car) => (
+              <FeaturedCar car={car}></FeaturedCar>
+            ))}
+          </div>
         </div>
       </div>
     </div>
